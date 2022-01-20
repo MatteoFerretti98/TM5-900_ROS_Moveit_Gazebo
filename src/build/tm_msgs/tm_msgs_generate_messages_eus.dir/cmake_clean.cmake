a8a8d7498909e8edfed7bef75b2cfb1774c2c030
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 22:48:52 +0100

    Add files via upload

---
+++ src/build/tm_msgs/tm_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tm_msgs_generate_messages_eus"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/msg/SctResponse.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/msg/SvrResponse.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/msg/StaResponse.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/msg/FeedbackState.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/SetPositions.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/SendScript.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/SetIO.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/SetEvent.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/ConnectTM.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/AskItem.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/AskSta.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/srv/WriteItem.l"
+  "/home/progettodcirv/catkin_ws/src/devel/share/roseus/ros/tm_msgs/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tm_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
