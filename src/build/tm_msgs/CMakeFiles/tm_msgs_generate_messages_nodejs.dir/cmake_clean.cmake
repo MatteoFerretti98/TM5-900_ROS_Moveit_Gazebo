577b6b0fe9254d641471e22aa40dbf32b2755aa6
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 22:54:46 +0100

    Add files via upload

---
+++ src/build/tm_msgs/CMakeFiles/tm_msgs_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tm_msgs_generate_messages_nodejs"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/msg/SctResponse.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/msg/SvrResponse.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/msg/StaResponse.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/msg/FeedbackState.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/SetPositions.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/SendScript.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/SetIO.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/SetEvent.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/ConnectTM.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/AskItem.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/AskSta.js"
+  "/home/progettodcirv/catkin_ws/src/devel/share/gennodejs/ros/tm_msgs/srv/WriteItem.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tm_msgs_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/tm_msgs/CMakeFiles/tm_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tm_msgs_generate_messages_py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/msg/_SctResponse.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/msg/_SvrResponse.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/msg/_StaResponse.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/msg/_FeedbackState.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_SetPositions.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_SendScript.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_SetIO.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_SetEvent.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_ConnectTM.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_AskItem.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_AskSta.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/_WriteItem.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/msg/__init__.py"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/python2.7/dist-packages/tm_msgs/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tm_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
