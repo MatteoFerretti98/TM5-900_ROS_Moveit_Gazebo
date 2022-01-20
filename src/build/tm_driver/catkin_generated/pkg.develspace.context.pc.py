32a49918f8759bfa89ac5eb596f5e1ee695414b8
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 18:55:34 +0100

    Add files via upload

---
+++ src/build/tm_driver/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/progettodcirv/catkin_ws/src/src/tm_driver/include".split(';') if "/home/progettodcirv/catkin_ws/src/src/tm_driver/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "actionlib;control_msgs;geometry_msgs;roscpp;sensor_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "tm_driver"
+PROJECT_SPACE_DIR = "/home/progettodcirv/catkin_ws/src/devel"
+PROJECT_VERSION = "1.1.2"
