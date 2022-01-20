fa0e2456eea758c1622448ddb6ef3ea879e83b74
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 18:40:36 +0100

    Add files via upload

---
+++ src/devel/share/gennodejs/ros/tm_msgs/msg/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let FeedbackState = require('./FeedbackState.js');
+let SctResponse = require('./SctResponse.js');
+let StaResponse = require('./StaResponse.js');
+let SvrResponse = require('./SvrResponse.js');
+
+module.exports = {
+  FeedbackState: FeedbackState,
+  SctResponse: SctResponse,
+  StaResponse: StaResponse,
+  SvrResponse: SvrResponse,
+};
---
+++ src/devel/share/gennodejs/ros/tm_msgs/srv/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let AskSta = require('./AskSta.js')
+let ConnectTM = require('./ConnectTM.js')
+let SetPositions = require('./SetPositions.js')
+let AskItem = require('./AskItem.js')
+let SetIO = require('./SetIO.js')
+let WriteItem = require('./WriteItem.js')
+let SendScript = require('./SendScript.js')
+let SetEvent = require('./SetEvent.js')
+
+module.exports = {
+  AskSta: AskSta,
+  ConnectTM: ConnectTM,
+  SetPositions: SetPositions,
+  AskItem: AskItem,
+  SetIO: SetIO,
+  WriteItem: WriteItem,
+  SendScript: SendScript,
+  SetEvent: SetEvent,
+};
