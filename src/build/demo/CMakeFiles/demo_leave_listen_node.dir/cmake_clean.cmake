f27003636da7483c53f1c4ce021e3db560290641
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 22:28:47 +0100

    Add files via upload

---
+++ src/build/demo/CMakeFiles/demo_leave_listen_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_leave_listen_node.dir/src/demo_leave_listen_node.cpp.o"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/demo/demo_leave_listen_node.pdb"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/demo/demo_leave_listen_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_leave_listen_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/demo/CMakeFiles/demo_set_event.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_set_event.dir/src/demo_set_event.cpp.o"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/demo/demo_set_event.pdb"
+  "/home/progettodcirv/catkin_ws/src/devel/lib/demo/demo_set_event"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_set_event.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
