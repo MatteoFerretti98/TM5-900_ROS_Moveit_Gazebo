69e0fd4ad6399a292eae3d796c7860dfa89f1ccd
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 22:22:46 +0100

    Add files via upload

---
+++ build/demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/progettodcirv/catkin_ws/src/src/demo
+# Build directory: /home/progettodcirv/catkin_ws/src/build/demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
