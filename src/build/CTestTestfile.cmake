792360d47d95ffff777e79f1789b8c6b3d3c56db
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2022-01-20 22:37:24 +0100

    Add files via upload

---
+++ src/build/CTestTestfile.cmake
@@ -0,0 +1,19 @@
+# CMake generated Testfile for 
+# Source directory: /home/progettodcirv/catkin_ws/src/src
+# Build directory: /home/progettodcirv/catkin_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("techman_robot")
+subdirs("tm12_description")
+subdirs("tm5_description")
+subdirs("tmr_description")
+subdirs("tm_msgs")
+subdirs("demo")
+subdirs("tm_demo")
+subdirs("tm_driver")
+subdirs("ui_for_debug_and_demo")
+subdirs("tm12_moveit_config")
+subdirs("tm5_700_moveit_config")
+subdirs("tm5_900_moveit_config")
---
+++ src/build/tmr_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/progettodcirv/catkin_ws/src/src/tmr_description
+# Build directory: /home/progettodcirv/catkin_ws/src/build/tmr_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
